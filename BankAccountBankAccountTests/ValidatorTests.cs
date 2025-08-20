c0918319e575c1103a21d2cebbbdc1cb96f46e1f
CW-ISM <cwismgitism@gmail.com>
2025-08-20 01:13:35 -0700

    Updated ValidatorTests.cs to use DataRow attributes for multiple test cases

--- BankAccountBankAccountTests/ValidatorTests.cs
+++ BankAccountBankAccountTests/ValidatorTests.cs
@@ -12,85 +12,90 @@ namespace BankAccount.BankAccountTests;
 public class ValidatorTests
 {
     [TestMethod()]
-    public void IsWithinRange_MinInclusiveBound_ReturnsTrue()
+    [DataRow(10, 10)]
+    [DataRow(5.001, 5.001)]
+    [DataRow(0.000001, 0.000001)]
+    public void IsWithinRange_MinInclusiveBound_ReturnsTrue(double minBoundary, double valueToCheck)
     {
         // Arrange
         Validator validator = new Validator();
-        double value = 5.0;
-        double min = 5.0;
-        double max = 10.0;
+        double maxBoundary = minBoundary + 100;
         // Act
-        bool result = validator.IsWithinRange(value, min, max);
+        bool result = validator.IsWithinRange(valueToCheck, minBoundary, maxBoundary);
         // Assert
         Assert.IsTrue(result);
     }
 
     [TestMethod()]
-    public void IsWithinRange_MaxInclusiveBound_ReturnsTrue()
+    [DataRow(10, 10)]
+    [DataRow(1000, 1000)]
+    [DataRow(0.000001, 0.000001)]
+    public void IsWithinRange_MaxInclusiveBound_ReturnsTrue(double maxBoundary, double valueToCheck)
     {
         // Arrange
         Validator validator = new Validator();
-        double value = 10.0;
-        double min = 5.0;
-        double max = 10.0;
+        double minBoundary = maxBoundary - 100;
         // Act
-        bool result = validator.IsWithinRange(value, min, max);
+        bool result = validator.IsWithinRange(valueToCheck, minBoundary, maxBoundary);
         // Assert
         Assert.IsTrue(result);
     }
 
     [TestMethod()]
-    public void IsWithinRange_WithinRange_ReturnsTrue()
+    [DataRow(10, 15, 12.5)]
+    [DataRow(-1, 1, 0.5)]
+    [DataRow(0, 100, 50)]
+    public void IsWithinRange_WithinRange_ReturnsTrue(double minBoundary, double maxBoundary, double valueToCheck)
     {
         // Arrange
         Validator validator = new Validator();
-        double value = 7.5;
-        double min = 5.0;
-        double max = 10.0;
         // Act
-        bool result = validator.IsWithinRange(value, min, max);
+        bool result = validator.IsWithinRange(valueToCheck, minBoundary, maxBoundary);
         // Assert
         Assert.IsTrue(result);
     }
 
     [TestMethod()]
-    public void IsWithinRange_ValueLessThanMin_ReturnsFalse()
+    [DataRow(1, -1)]
+    [DataRow(0, -10)]
+    [DataRow(100, 99)]
+    public void IsWithinRange_ValueLessThanMin_ReturnsFalse(double minBoundary, double valueToCheck)
     {
         // Arrange
         Validator validator = new Validator();
-        double value = 4.0;
-        double min = 5.0;
-        double max = 10.0;
+        double maxBoundary = minBoundary + 100;
         // Act
-        bool result = validator.IsWithinRange(value, min, max);
+        bool result = validator.IsWithinRange(valueToCheck, minBoundary, maxBoundary);
         // Assert
         Assert.IsFalse(result);
     }
 
     [TestMethod()]
-    public void IsWithinRange_ValueGreaterThanMax_ReturnsFalse()
+    [DataRow(1, 2)]
+    [DataRow(0.01, 0.10)]
+    [DataRow(100, 101)]
+    public void IsWithinRange_ValueGreaterThanMax_ReturnsFalse(double maxBoundary, double valueToCheck)
     {
         // Arrange
         Validator validator = new Validator();
-        double value = 11.0;
-        double min = 5.0;
-        double max = 10.0;
+        double minBoundary = maxBoundary - 100;
         // Act
-        bool result = validator.IsWithinRange(value, min, max);
+        bool result = validator.IsWithinRange(valueToCheck, minBoundary, maxBoundary);
         // Assert
         Assert.IsFalse(result);
     }
 
     [TestMethod()]
-    public void IsWithinRange_MinGreaterThanMax_ThrowsArgumentException()
+    [DataRow(15.0, 10.0)]
+    [DataRow(100.0, 50.0)]
+    [DataRow(0.0001, 0.00001)]
+    public void IsWithinRange_MinGreaterThanMax_ThrowsArgumentException(double minBoundary, double maxBoundary)
     {
         // Arrange
         Validator validator = new Validator();
-        double value = 5.0;
-        double min = 10.0;
-        double max = 5.0;
+        double valueToCheck = 0.0;
         // Act & Assert
         Assert.ThrowsExactly<ArgumentException>(() =>
-            validator.IsWithinRange(value, min, max));
+            validator.IsWithinRange(valueToCheck, minBoundary, maxBoundary));
     }
 }
