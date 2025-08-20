ca6203ce5023722ea8f47bfdf0d0db559376548a
CW-ISM <cwismgitism@gmail.com>
2025-08-19 22:08:06 -0700

    Added function IsWithInRange

--- BankAccount/Validator.cs
+++ BankAccount/Validator.cs
@@ -8,5 +8,20 @@ namespace BankAccount;
 
 public class Validator
 {
-    
+    /// <summary>
+    /// Checks to ensure a value is between inclusive range.
+    /// </summary>
+    /// <param name="value">Value to check</param>
+    /// <param name="min">Minimum inclusive boundary</param>
+    /// <param name="max">Maximum inclusive boundary</param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    public bool IsWithinRange(double value, double min, double max)
+    {
+        if (min > max)
+        {
+            throw new ArgumentException("Minimum value cannot be greater than maximum value.");
+        }
+        return value >= min && value <= max;
+    }
 }
