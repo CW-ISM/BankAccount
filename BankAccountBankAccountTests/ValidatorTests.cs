f80951d28fc07c3bd60f66a9ea38714c672c6c63
CW-ISM <cwismgitism@gmail.com>
2025-08-20 00:57:25 -0700

    Added multiple tests to ValidatorTests.cs

--- BankAccountBankAccountTests/ValidatorTests.cs
+++ BankAccountBankAccountTests/ValidatorTests.cs
@@ -12,8 +12,85 @@ namespace BankAccount.BankAccountTests;
 public class ValidatorTests
 {
     [TestMethod()]
-    public void IsWithinRangeTest()
+    public void IsWithinRange_MinInclusiveBound_ReturnsTrue()
     {
-        Assert.Fail();
+        // Arrange
+        Validator validator = new Validator();
+        double value = 5.0;
+        double min = 5.0;
+        double max = 10.0;
+        // Act
+        bool result = validator.IsWithinRange(value, min, max);
+        // Assert
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod()]
+    public void IsWithinRange_MaxInclusiveBound_ReturnsTrue()
+    {
+        // Arrange
+        Validator validator = new Validator();
+        double value = 10.0;
+        double min = 5.0;
+        double max = 10.0;
+        // Act
+        bool result = validator.IsWithinRange(value, min, max);
+        // Assert
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod()]
+    public void IsWithinRange_WithinRange_ReturnsTrue()
+    {
+        // Arrange
+        Validator validator = new Validator();
+        double value = 7.5;
+        double min = 5.0;
+        double max = 10.0;
+        // Act
+        bool result = validator.IsWithinRange(value, min, max);
+        // Assert
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod()]
+    public void IsWithinRange_ValueLessThanMin_ReturnsFalse()
+    {
+        // Arrange
+        Validator validator = new Validator();
+        double value = 4.0;
+        double min = 5.0;
+        double max = 10.0;
+        // Act
+        bool result = validator.IsWithinRange(value, min, max);
+        // Assert
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod()]
+    public void IsWithinRange_ValueGreaterThanMax_ReturnsFalse()
+    {
+        // Arrange
+        Validator validator = new Validator();
+        double value = 11.0;
+        double min = 5.0;
+        double max = 10.0;
+        // Act
+        bool result = validator.IsWithinRange(value, min, max);
+        // Assert
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod()]
+    public void IsWithinRange_MinGreaterThanMax_ThrowsArgumentException()
+    {
+        // Arrange
+        Validator validator = new Validator();
+        double value = 5.0;
+        double min = 10.0;
+        double max = 5.0;
+        // Act & Assert
+        Assert.ThrowsExactly<ArgumentException>(() =>
+            validator.IsWithinRange(value, min, max));
     }
 }
