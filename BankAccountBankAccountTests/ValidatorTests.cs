220dd1d5d256a82d842f8dc40f239585fe5be8d4
CW-ISM <cwismgitism@gmail.com>
2025-08-19 22:23:48 -0700

    Upgraded packages

---
+++ BankAccountBankAccountTests/ValidatorTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccount;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount.BankAccountTests;
+
+[TestClass()]
+public class ValidatorTests
+{
+    [TestMethod()]
+    public void IsWithinRangeTest()
+    {
+        Assert.Fail();
+    }
+}
